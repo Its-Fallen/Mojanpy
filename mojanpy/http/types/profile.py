2dcade9762e45fb77c94b1d7b13de192cf0abb51
Its-Fallen <git.fajzievm1@gmail.com>
2022-02-19 20:30:18 +0300

    update black

--- mojanpy/http/types/profile.py
+++ mojanpy/http/types/profile.py
@@ -4,45 +4,38 @@ import base64
 import json
 
 
-
-class Profile:
+class DefaultProfile:
     def __init__(self, name: str, uuid: str, properties: List, **kwargs):
         self.__name = name
         self.__uuid = uuid
         self.__properties = properties[0]
-        self.__isLegacy = None
-
 
     @property
     def name(self):
         return self.__name
 
-
     @property
     def uuid(self):
         return self.__uuid
-
 
     @property
     def properties(self):
         return self.__properties
 
-
     @property
     def skin(self):
-        data = json.loads(base64.b64decode(self.__properties["value"]).decode())                            
+        data = json.loads(base64.b64decode(self.__properties["value"]).decode())
         try:
             url = data["textures"]["SKIN"]["url"]
         except:
-            url = None 
+            url = None
         return url
 
-    
     @property
     def cape(self):
         data = json.loads(base64.b64decode(self.__properties["value"]).decode())
         try:
             url = data["textures"]["CAPE"]["url"]
         except:
-            url = None 
+            url = None
         return url
